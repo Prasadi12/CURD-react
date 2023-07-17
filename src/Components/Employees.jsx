81f01e25e61d7d430c38d9d692e9f21939ab0b14
A.N.P Jayamanna <niluprasadi12@gmail.com>
2023-07-17 16:05:37 +0530

    first commit

---
+++ src/Components/Employees.jsx
@@ -0,0 +1,19 @@
+const Employees = [
+    {
+        id: '1',
+        name: 'Prasadi',
+        age: '25',
+    },
+    {
+        id: '2',
+        name: 'Nishadi',
+        age: '27',
+    },
+    {
+        id: '3',
+        name: 'Sithara',
+        age: '29',
+    },
+];
+
+export default Employees;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+import { Button, Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
